04dafa5d3af34068a875676a2a365cb4c2c51b02
michaeldcanady <michaeldcanady+github@gmail.com>
2022-04-05 22:08:54 -0400

    Adding decorators for required and optional args

--- src/userservice/userservice.py
+++ src/userservice/userservice.py
@@ -15,11 +15,39 @@ import re
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.serialization import load_pem_private_key, load_pem_public_key
 
-class TokenError(Exception):
-    pass
 
-class ArgumentError(Exception):
-    pass
+class APIException(Exception):
+    def __init__(self, message, error_code):
+        self.message = message
+        self.code = error_code
+
+
+class TokenError(APIException):
+    def __init__(self, message, error_code):
+        super().__init__(message, error_code)
+
+
+class ArgumentError(APIException):
+    def __init__(self, message, error_code):
+        super().__init__(message, error_code)
+
+
+class EndpointPermissionError(APIException):
+    def __init__(self, message, error_code):
+        super().__init__(message, error_code)
+
+
+class SQLLookupError(APIException):
+    def __init__(self, message, error_code):
+        super().__init__(message, error_code)
+
+class CategoryLookupError(APIException):
+    def __init__(self, message, error_code):
+        super().__init__(message, error_code)
+
+class BudgetLookupError(APIException):
+    def __init__(self, message, error_code):
+        super().__init__(message, error_code)
 
 
 def create_app():
@@ -28,8 +56,22 @@ def create_app():
     """
     app = Flask(__name__)
 
-    # Register Beta version of the API
-    # beta.register_version(app)
+    def exception_handler(func):
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                error_code = getattr(e, "code", 500)
+                message = getattr(e, "message", "Uknown exception {0}".format(str(e)))
+                app.logger.exception("API exception: %s", message)
+                error_response = {
+                    "message": message,
+                    "error_code": error_code,
+                }
+                return jsonify(error_response), error_code
+        # Renaming the function name:
+        wrapper.__name__ = func.__name__
+        return wrapper
 
     def requires_token(func):
         """Used to check if the path has a token
@@ -37,12 +79,12 @@ def create_app():
         Args:
             func (function): _description_
         """
-        def _requires_token(*args, **kwargs):
+        def _requires_header_token(*args, **kwargs):
             try:
                 raw_token = request.headers.get(app.config['TOKEN_NAME'])
 
                 if raw_token is None:
-                    raise TokenError("No token provided")
+                    raise TokenError("No token provided", 401)
 
                 # Encodes token for verification
                 token = raw_token.encode('utf8')
@@ -50,53 +92,84 @@ def create_app():
                 if not verify_token(token):
                     raise UserWarning("user is not authenticated")
 
-                func(*args, **kwargs)
+                return func(*args, **kwargs)
             except UserWarning as e:
                 app.logger.error("Error validating token: %s", str(e))
                 return str(e), 401
+        # Renaming the function name:
+        _requires_header_token.__name__ = func.__name__
+        return _requires_header_token
 
-        return _requires_token
+    def validate_args(allowed_args: list):
+        def decorator(func):
+            def _validate_args(*args, **kwargs):
+
+                kwargs["all_provided_args"] = request.args
+
+                if any(arg not in allowed_args for arg in list(kwargs["all_provided_args"].keys())):
+                    raise ArgumentError(
+                        'Unaccepted argument(s) for provided', 420)
+
+                return func(*args, **kwargs)
+            # Renaming the function name:
+            decorator.__name__ = func.__name__
+            return _validate_args
+        # Renaming the function name:
+        validate_args.__name__ = decorator.__name__
+        return decorator
+
+    def required_args(require_args: list):
+        def required_args_decorator(func):
+            def _required_args(*args, **kwargs):
+
+                kwargs["all_provided_args"] = request.args
+
+                # Checks if required args are in list
+                if not any(req_arg not in list(kwargs["all_provided_args"].keys()) for req_arg in require_args) or len(kwargs["all_provided_args"]) == 0:
+                    raise ArgumentError('Missing required argument(s)', 419)
+
+                return func(*args, **kwargs)
+            # Renaming the function name:
+            _required_args.__name__ = func.__name__
+            return _required_args
+        # Renaming the function name:
+        required_args.__name__ = required_args_decorator.__name__
+        return required_args_decorator
 
     @app.route("/signin", methods=["Get"])
+    @exception_handler
     def signin():
         app.logger.debug('Sanitizing login input.')
-        try:
-            raw_email = request.args.get('email')
+        raw_email = request.args.get('email')
 
-            if raw_email is None:
-                raise ValueError("No email argument provided")
+        if raw_email is None:
+            raise ArgumentError("No email argument provided", 419)
 
-            raw_password = request.args.get('password')
+        raw_password = request.args.get('password')
 
-            if raw_password is None:
-                raise ValueError("No password argument provided")
+        if raw_password is None:
+            raise ArgumentError("No password argument provided", 419)
 
-            remember_me = request.form.get('rememberme', False, bool)
+        remember_me = request.form.get('rememberme', False, bool)
 
-            email = bleach.clean(raw_email)
-            password = bleach.clean(raw_password)
+        email = bleach.clean(raw_email)
+        password = bleach.clean(raw_password)
 
-            user = users_db.get_user(email)
-            if user is None:
-                raise LookupError('user {0} does not exist'.format(email))
+        user = users_db.get_user(email)
+        if user is None:
+            raise SQLLookupError('user {0} does not exist'.format(email), 404)
 
-            # Validate the password
-            if not bcrypt.checkpw(password.encode('utf-8'), user["passhash"]):
-                raise PermissionError('Invalid login')
+        # Validate the password
+        if not bcrypt.checkpw(password.encode('utf-8'), user["passhash"]):
+            raise EndpointPermissionError('Invalid login', 401)
 
-            # Generates token
-            token = __generate_new_token(user)
+        # Generates token
+        token = __generate_new_token(user)
 
-            return jsonify({'token': token.decode("utf-8")}), 200
-
-        except LookupError as err:
-            return str(err), 404
-        except PermissionError as err:
-            return str(err), 401
-        except ValueError as err:
-            return str(err), 400
+        return jsonify({'token': token.decode("utf-8")}), 200
 
     @app.route("/signup", methods=["POST"])
+    @exception_handler
     def signup():
         try:
             app.logger.debug('Sanitizing input.')
@@ -144,84 +217,69 @@ def create_app():
             return str(e), 404
 
     @app.route("/<user_id>/budgets", methods=["POST"])
+    @exception_handler
     @requires_token
     def add_users_budgets(user_id: str):
-        try:
-            allowed_args = []
 
-            all_provided_args = request.args.listvalues()
+        # get display name from form
+        raw_displayname = request.form.get('displayname', None, str)
+        # get budget notes from form
+        raw_budgetnotes = request.form.get('notes', "", str)
 
-            #__validate_provided_args(all_provided_args, allowed_args)
+        if raw_displayname is None:
+            raise ArgumentError("No display name argument provided", 419)
 
-            # get display name from form
-            raw_displayname = request.form.get('displayname', None, str)
-            # get budget notes from form
-            raw_budgetnotes = request.form.get('notes', "", str)
+        budgetid = budgets_db.generate_budgetid()
+        budgetnotes = bleach.clean(raw_budgetnotes)
+        displayname = bleach.clean(raw_displayname)
 
-            if raw_displayname is None:
-                raise ValueError("No display name argument provided")
+        #auth_payload = get_token_data(request.headers.get(app.config['TOKEN_NAME']).encode('utf8'))
 
-            budgetid = budgets_db.generate_budgetid()
-            budgetnotes = bleach.clean(raw_budgetnotes)
-            displayname = bleach.clean(raw_displayname)
+        # if user_id != auth_payload["userid"]:
+        #    raise PermissionError
 
-            #auth_payload = get_token_data(request.headers.get(app.config['TOKEN_NAME']).encode('utf8'))
+        budget_data = {
+            "budgetid": budgetid,
+            "displayname": displayname,
+            "budgetnotes": budgetnotes,
+            "accessdate": datetime.utcnow(),
+            "userid": user_id,
+        }
 
-            #if user_id != auth_payload["userid"]:
-            #    raise PermissionError
+        # Add user_data to database
+        app.logger.debug("Adding budget to the database")
+        budgets_db.add_budget(budget_data)
+        app.logger.info("Successfully created budget.")
 
-            budget_data = {
-                "budgetid": budgetid,
-                "displayname": displayname,
-                "budgetnotes": budgetnotes,
-                "accessdate": datetime.utcnow(),
-                "userid": user_id,
-            }
+        budget = budgets_db.get_budget(budget_data["budgetid"])
 
-            # Add user_data to database
-            app.logger.debug("Adding budget to the database")
-            budgets_db.add_budget(budget_data)
-            app.logger.info("Successfully created budget.")
-
-            budget = budgets_db.get_budget(budget_data["budgetid"])
-
-            return jsonify(budget), 201
-        except ArgumentError as e:
-            app.logger.error("Error creating budget: %s", str(e))
-            return str(e), 401
-
-        except UserWarning as e:
-            app.logger.error("Error validating token: %s", str(e))
-            return str(e), 401
-        
-        except TokenError as e:
-            app.logger.error("Error validating token: %s", str(e))
-            return str(e), 404
-
-        except ValueError as e:
-            app.logger.error("Error creating budget: %s", str(e))
-            return str(e), 401
-
-        except Exception as e:
-            app.logger.error("Unknown error creating budget: %s", str(e))
-            return str(e), 500
+        return jsonify(budget), 201
 
     @app.route("/<user_id>/budgets", methods=["GET"])
-    def get_users_budgets(user_id: str):
-
+    @exception_handler
+    @requires_token
+    @validate_args(["count"])
+    def get_users_budgets(user_id: str, **kwargs):
         try:
             # Get all budgets that belong to this user
             app.logger.debug("fetching budgets of %s", user_id)
             budgets = budgets_db.get_budgets(user_id)
             app.logger.debug("successfully got budgets")
 
-            return jsonify({"values": budgets}), 201
+            dict_return = {"values": budgets}
+
+            if ("count" in list(kwargs["all_provided_args"].keys())) and (kwargs["all_provided_args"]["count"] == "true"):
+                dict_return["count"] = len(budgets)
+
+            return jsonify(dict_return), 201
 
         except SQLAlchemyError as err:
             app.logger.error("Error fetching budgets: %s", str(err))
             return 'failed to fetch budgets', 500
 
     @app.route("/<user_id>/budgets/<budget_id>", methods=["GET"])
+    @exception_handler
+    @requires_token
     def get_users_budget(user_id: str, budget_id: str):
         try:
             # Get all budget with the specified id
@@ -242,6 +300,8 @@ def create_app():
             return 'failed to fetch budgets', 500
 
     @app.route("/<user_id>/budgets/<budget_id>", methods=["PATCH"])
+    @exception_handler
+    @requires_token
     def update_users_budget(user_id: str, budget_id: str):
 
         try:
@@ -271,58 +331,39 @@ def create_app():
             return 'failed to update budget', 500
 
     @app.route("/<user_id>/budgets/<budget_id>", methods=["DELETE"])
+    @exception_handler
+    @requires_token
     def remove_users_budget(user_id: str, budget_id: str):
-        try:
 
-            if budgets_db.get_budget(budget_id) is None:
-                raise LookupError(
-                    "budget with id {0} not found".format(budget_id))
+        if budgets_db.get_budget(budget_id) is None:
+            raise BudgetLookupError("budget with id {0} not found".format(budget_id), 404)
 
-            app.logger.debug("Deleting budget with id %s", budget_id)
-            budgets_db.delete_budget(budget_id)
-            app.logger.debug("Successfully deleted budget")
+        app.logger.debug("Deleting budget with id %s", budget_id)
+        budgets_db.delete_budget(budget_id)
+        app.logger.debug("Successfully deleted budget")
 
-            budget = budgets_db.get_budget(budget_id)
+        budget = budgets_db.get_budget(budget_id)
 
-            return jsonify({}), 201
-
-        except LookupError as err:
-            app.logger.error("Error deleting budget: %s", str(err))
-            return str(err), 404
-
-        except SQLAlchemyError as err:
-            app.logger.error("Error creating new user: %s", str(err))
-            return 'failed to delete budget', 500
+        return jsonify({}), 201
 
     @app.route('/<user_id>/budgets/<budget_id>/categories', methods=["POST"])
-    def add_budget_category(user_id: str, budget_id: str):
-        try:
-
-            #token = request.cookies.get(app.config['TOKEN_NAME'])
-            # if not verify_token(token):
-            #    raise UserWarning("user is not authenticated")
-
+    @exception_handler
+    @requires_token
+    @required_args('displayname')
+    def add_budget_category(user_id: str, budget_id: str, **kwargs):
             #auth_payload = get_token_date()
-
-            raw_displayname = request.args.get('displayname', None, str)
-
-            if raw_displayname is None:
-                raise ValueError("No display name argument provided")
 
             raw_parentid = request.args.get('parentid', None, str)
 
             categoryid = categories_db.generate_categoryid()
-            displayname = bleach.clean(raw_displayname)
+            displayname = bleach.clean(kwargs["all_provided_args"]['displayname'])
 
             parentid = bleach.clean(
                 raw_parentid) if raw_parentid is not None else None
 
             if (parentid is not None) and (categories_db.get_category(parentid) is None):
-                raise LookupError(
-                    "Unable to find category with Id %s", parentid)
-
-            # if user_id != auth_payload["userid"]:
-            #    raise PermissionError
+                raise CategoryLookupError(
+                    "Unable to find category with Id {0}".format(parentid), 401)
 
             category_data = {
                 "categoryid": categoryid,
@@ -339,21 +380,6 @@ def create_app():
             budget = categories_db.get_category(category_data["categoryid"])
 
             return jsonify(budget), 201
-        except LookupError as e:
-            app.logger.error("Error creating category: %s", str(e))
-            return str(e), 401
-
-        except UserWarning as e:
-            app.logger.error("Error validating token: %s", str(e))
-            return str(e), 401
-
-        except ValueError as e:
-            app.logger.error("Error creating category: %s", str(e))
-            return str(e), 401
-
-        except SQLAlchemyError as err:
-            app.logger.error("Error creating new category: %s", str(err))
-            return 'failed to create category', 500
 
     def verify_token(token):
         """
@@ -408,10 +434,6 @@ def create_app():
         # Verify username contains only 2-15 alphanumeric or underscore characters
         # if not re.match(r"\A[a-zA-Z0-9_]{2,15}\Z", req['username']):
             #raise UserWarning('username must contain 2-15 alphanumeric characters or underscores')
-
-    def __validate_provided_args(provide_args: dict, accepted_args: list):
-        if any(arg not in accepted_args for arg in list(provide_args.keys())):
-            raise ArgumentError('Unaccepted argument(s) for provided')
 
     def __generate_new_token(user: dict) -> bytes:
         exp_time = datetime.utcnow() + \
