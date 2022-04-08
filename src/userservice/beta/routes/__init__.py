11dbfadf5b8e6bce24565fecf74e6e1b28fc2769
michaeldcanady <michaeldcanady+github@gmail.com>
2022-04-07 22:27:17 -0400

    refactoring for clearing API routing

--- src/userservice/beta/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from beta.routes import signin
-
-def register_routes(app,parent):
-    #budgets.register_route(parent)
-    #categories.register_route(parent)
-    #users.register_route(parent)
-    signin.register_route(app, parent)
