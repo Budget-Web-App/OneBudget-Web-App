6cd1f70bea2e5c7bd674cd383177cca3c8e8ff11
michaeldcanady <michaeldcanady+github@gmail.com>
2022-04-26 18:04:54 -0400

    Drop files from .gitignore

--- app/mod_api/beta/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.mod_api.beta.routes import budgets, categories, users, signin
-
-def register_routes(parent):
-    budgets.register_route(parent)
-    categories.register_route(parent)
-    users.register_route(parent)
-    signin.register_route(parent)
--- app/mod_front/common.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-
-view = Blueprint("view", __name__, url_prefix="/")
